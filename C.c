dd320692106ee8859da96d8d468e6b2eea83c811
captjay98 <captjay98@gmail.com>
2022-02-19 04:22:39 +0100

    Pushing

---
+++ C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    // A comment
+    /*
+      a ulti line comment
+    */
+
+    printf("I \n am Finally Learning \nC");
+    printf("And\t it's not as bad as i thought");
+
+    return 0;
+}
+
+C is Difficult. but i shall conquer it in sha Allah.
