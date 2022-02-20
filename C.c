196502a463409fe5c435fa0d490bf8681f41e50b
captjay98 <captjay98@gmail.com>
2022-02-21 00:00:39 +0100

    Making Progress in C

--- C.c
+++ C.c
@@ -7,10 +7,8 @@ int main(){
       a ulti line comment
     */
 
-    printf("I \n am Finally Learning \nC");
-    printf("And\t it's not as bad as i thought");
+    printf("I am Finally Learning C \n");
+    printf("And it is not as bad as I thought");
 
     return 0;
 }
-
-C is Difficult. but i shall conquer it in sha Allah.
