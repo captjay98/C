0f88b334731d1e4abb425a2de2c4bbc0dd764f16
captjay98 <captjay98@gmail.com>
2022-03-05 01:54:01 +0100

    Keeping my streak alive

--- C.c
+++ C.c
@@ -4,6 +4,28 @@
 
 int main(){
 
+    char unit;
+    float temp;
+    printf("Enter Unit: \n");
+    scanf("%c". &unit);
+
+    unit = toupper(unit);
+
+    if (unit =='C'){
+        printf("The temperature is currently in Celcius\n");
+    }
+    else if (unit =='F'){
+        printf("The temperature is currently in Farenheit\n");
+        
+    }
+
+
+
+
+
+
+
+
  
     return 0;
 
