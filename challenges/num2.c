ab7d12a7afd7242098c7a5d1901880eef1c97e3d
captjay98 <captjay98@gmail.com>
2022-04-24 23:11:21 +0100

    NAILED a challenge

---
+++ challenges/num2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    int i, n, nt;
+
+    puts("Num    Times2");
+    puts("___    ______");
+
+
+    for ( i = 0; i <=10; i++)
+
+    {
+        n +=1;
+        nt = n *2;
+
+    
+    printf(" %2i      %2i\n",n, nt);
+    }
+return 0;
+    
+}
