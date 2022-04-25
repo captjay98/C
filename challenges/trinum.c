61421018b164772f115d3cf2dca57a95abeb0a03
captjay98 <captjay98@gmail.com>
2022-04-25 01:05:41 +0100

    Made more changes to added files

--- challenges/trinum.c
+++ challenges/trinum.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 int main (void)
 {
-     int  n, num, count, triangularNumber;
-     count = 1;
+     int  n, num, count = 1, triangularNumber;
+
      while (count <= 5) 
      {
           printf ("What triangular num do you want? ");
           scanf  ("%i", &num);
+          
           triangularNumber = 0;
           for ( n = 1;  n <= num;  ++n )
                triangularNumber += n;
           printf ("Triangular num %i is %i\n\n", num, triangularNumber);
-     }++count;
+          count++;
+     }
      return 0;
 }
+
+
+
+
