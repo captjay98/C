904ccbb1ec85f9e9f58ae9ed7d32665e7205f7f1
captjay98 <captjay98@gmail.com>
2022-05-09 04:59:37 +0100

    GOT IT AT LAST

---
+++ challenges/gradeexarr.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int numberofGrades , i, grade, sum = 0;
+    int gradetotal[5];
+    int failurecount = 0;
+    float average;
+    
+
+    puts("Enter Total grades");
+    scanf  ("%i", &numberofGrades);
+
+    for (i = 1; i <= 100; i++)
+        gradetotal[i] = 0;
+
+   
+
+    for (i = 1; i <= numberofGrades; i++)
+    {
+        printf("Enter grade %i\n", i);
+        scanf ("%i", &grade);
+
+        gradetotal[i] = grade;
+
+        sum += gradetotal[i];
+
+        if (grade < 65)
+        ++failurecount;
+
+        
+    }
+    average = (float) sum / numberofGrades;
+
+    for ( i = 1; i <= 5; i++)
+         printf("%4i%14i\n", i, gradetotal[i]);
+
+    printf("Average = %.2f \n", average);
+    printf("Failures = %i\n", failurecount);
+    
+
+    return 0;
+}
