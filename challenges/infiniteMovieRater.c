204b59e77fd5bb956df15a2a79d9922887d25a96
captjay98 <captjay98@gmail.com>
2022-05-13 16:26:48 +0100

    Modified movie rater

---
+++ challenges/infiniteMovieRater.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+//Movie rater
+
+//Enter 999 to stop taking inout and display results
+
+int main(void)
+{
+    int ratingcounter[11], i, response;
+
+    for (i = 1; i <= 10; i++)
+    {
+        ratingcounter[i] = 0;
+    }
+
+    puts(" Rate on a scale of 10\n");
+
+    for (i = 1; ; ++i)
+    {
+        scanf("%i", &response);
+
+        if (response == 999)
+        {
+            break;
+        }
+
+       if (response < 1 || response > 10)
+        {
+            printf(" %i is Out of range\n", response);
+            puts(" Rate on a scale of 10\n");
+        }
+
+        else
+            {
+                ++ratingcounter[response];
+            }
+    }
+
+    puts("\n\nRating   Ratings\n");
+    puts("------   --------\n");
+
+    for ( i = 1;  i <= 10;  ++i )
+    {
+          printf ("%4i%10i\n", i, ratingcounter[i]);
+    }
+
+
+    return 0;
+}
