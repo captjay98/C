8347a6f4b31fd498bdc3fb05824d5694ec49d0c0
captjay98 <captjay98@gmail.com>
2022-05-15 00:53:08 +0100

    improvements

--- challenges/EveOddArray.c
+++ challenges/EveOddArray.c
@@ -6,26 +6,15 @@ int main (void)
 
      for (i = 1; i <= 100; i++)
      {
-         Values[i] = 0;
-     }
-
-     for (i = 1; i <= 100; i++)
-     {
-         if (i % 2 != 0)
          Values[i] = i;
-
-         else if (i % 2 == 0)
-         Values[i] = i;
-
-        
      }
 
      for (i = 1; i <= 100; i++)
      {
          if (Values[i] % 2 == 0)
-         printf("is Eve [%i] = %i \n", i, Values[i]);
+         printf("[%i] is Even  = %i \n", i, Values[i]);
          else
-         printf("is Odd [%i] = %i \n", i, Values[i]);
+         printf("[%i] is Odd   = %i \n", i, Values[i]);
 
      }
 
