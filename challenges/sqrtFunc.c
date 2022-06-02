21e117a849736571278dec1d046b1b7426f0d2a1
captjay98 <captjay98@gmail.com>
2022-06-02 02:12:19 +0100

    Squaeroot func

---
+++ challenges/sqrtFunc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+float absVal(float val);
+float sqroot(float val);
+
+int main(void)
+
+{
+    puts("Enter Number: ");
+
+    float y;
+    scanf("%f", &y);
+
+    double x = sqroot(y);
+
+    printf("Squareroot of %.2f = %.5lf ", y, x);
+
+}
+
+float absVal(float val)
+{
+    if (val < 0)
+        val = -val;
+    return val;
+}
+
+float sqroot(float val)
+{
+    float epsilon;
+    float guess = 1.0;
+    puts("Enter epsilon");
+    scanf("%f)", &epsilon);
+
+    if (val < 0)
+    {
+        puts("Negative num\n");
+        return - 1.0;
+    }
+
+    while (absVal(guess * guess - val) >= epsilon)
+        guess = (val / guess + guess) / 2.0;
+
+    return guess;
+}
