e426d139a1902f3d6045b6a678322bbd8be06ccc
captjay98 <captjay98@gmail.com>
2022-07-01 20:54:10 +0100

    Various ways to initialize Structures

--- challenges/initStruct.c
+++ challenges/initStruct.c
@@ -9,3 +9,15 @@ struct date today = { .year = 2015 };
 today = (struct date) { 9, 25, 2015 };
 today = (struct date) { .month = 9, .day = 25, .year = 2015 };
 nextDay =  dateUpdate ((struct date) { 5, 11, 2004} );
+struct  date
+{
+    int  month;
+    int  day;
+    int  year;
+} todaysDate = { 1, 11, 2005 };
+struct
+{
+    int  month;
+    int  day;
+    int  year;
+}  dates[100];
