e37a761ba8433a661e7b8d19aed1dff5be5524c6
captjay98 <captjay98@gmail.com>
2022-07-01 23:36:36 +0100

    Returns the Lenght of a string

---
+++ challenges/strLen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int strLen(const char string[]);
+
+int main()
+{
+    const char word5[] = {"Hello, world! "};
+    const char word6[] = {"Hey "};
+    const char word7[] = {"woof "};
+
+    printf(" len of %s = %i \n len of %s = %i \n len of %s = %i",
+            word5, strLen(word5), word6, strLen(word6), word7, strLen(word7));
+
+}
+
+int strLen(const char string[])
+{
+    int len = 0;
+    while( string[len] != '\0')
+    {
+        ++len;
+    }
+    return len;
+}
